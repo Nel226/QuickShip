da0c1b4be449f174e3c502780d84ab219fd5eaa9
SOME Barsour Evence <sourbar2001@gmail.com>
2023-08-21 00:35:09 +0200

    correction des test pour test_colis

--- tests/test_colis.py
+++ tests/test_colis.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
+from data.database import create_connection
 from source.colis import Colis
 
 
