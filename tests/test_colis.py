5dce617dd9202c5d8fbb1b9a63d16337867a581c
Nel226 <112627777+Nel226@users.noreply.github.com>
2023-08-21 00:36:46 +0200

    Insertion de la creation de la table colis

--- tests/test_colis.py
+++ tests/test_colis.py
@@ -5,7 +5,10 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from data.database import create_connection
 from source.colis import Colis
 
-
+# Créer la table "users" dans la base de données s'il n'existe pas déjà
+create_users_table()
+# Créer la table "COLIS" dans la base de données s'il n'existe pas déjà
+create_colis_table()
 
 def test_create_colis():
     colis = Colis.create_colis("SOME", "Bobo", "Judicael", "Karpala", 5.0)
