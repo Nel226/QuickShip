bdbd089ec04419d1e2a22ef7056914eb8e769f91
Nel226 <112627777+Nel226@users.noreply.github.com>
2023-08-21 00:40:39 +0200

    Suppression de la creation de la table users

--- tests/test_colis.py
+++ tests/test_colis.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from data.database import create_users_table, create_colis_table
 
 # Create the "users" and "colis" tables before running the tests
-create_users_table()
+
 create_colis_table()
 
 import pytest
